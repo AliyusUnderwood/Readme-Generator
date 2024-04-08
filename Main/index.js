b1af9c4af58eceacd7f89713c56fb2de2a08341f
AliyusUnderwood <158104889+AliyusUnderwood@users.noreply.github.com>
2024-04-08 15:55:37 -0700

    Created and coded all of the logic to create the readme and typed out the readme for repo

---
+++ Main/index.js
@@ -0,0 +1,89 @@
+// Included packages needed for this app
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require("./utils/generateMarkdown");
+
+//  Created an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What's your GitHub username?"
+  },
+
+  {
+    type: "input",
+    name: "email",
+    message: "What's your email?"
+  },
+
+  {
+    type: "input",
+    name: "title",
+    message: "What's the name of your project?"
+  },
+
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of your project:"
+  },
+
+  {
+    type: "input",
+    name: "github",
+    message: "What's you GitHub username?"
+  },
+
+  {
+    type: "list",
+    name: "license",
+    message: "What license does our project have?",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "GLP3.0", "BSD2", "BSD3", "None"]
+  },
+
+  {
+    type: "input",
+    name: "dependencies",
+    message: "Any dependencies to install?",
+    default: "npm i"
+  },
+
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the proper usage of this repo?"
+  },
+
+  {
+    type: "input",
+    name: "contributors",
+    message: "Who are the contributors of this repo?"
+  }
+];
+
+// Created a function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(),fileName), data);
+  }
+
+// Created a function to initialize app
+function init() {
+  inquirer.prompt(questions)
+  .then((userAnswers) => {
+    console.log("Generating...");
+    writeToFile("./sampleREADME.md", generateMarkdown({ ...userAnswers }));
+  })
+}
+
+// Function call to initialize app
+init();
+    
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Readme-Generator
+# Professional-Readme-Generator
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Goals](#goals)
+- [Technologies Used](#technologies-used)
+- [Screen Capture](#screen-capture)
+- [How To Use](#how-to-use)
+- [URL](#url)
+
+## Overview
+This repository is an exhibition of a professional README generator. The user can consult the repo to learn how to create a command-line application that dynamically generates a professional README.md file from a user's input using the <a href="https://www.npmjs.com/package/inquirer" target="_blank">Inquirer Package</a>.
+
+## Goals
+Allow users when making a project on Github, to be able to create a high-quality README.
+
+## Technologies Used
+<div style="display: inline_block"><br>
+  <img height="40" alt="Chris-Js" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-plain.svg">
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img height="40" alt="Chris-Node" height="30" width="40" src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg">
+ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+</br>
+
+## Screen Capture
+![](main/gif/Untitled_%20Apr%208,%202024%203_40%20PM%20(2).gif)
+
+## How To Use
+  1. Open integrated terminal in personal IDE.
+  2. Run ```node index.js```.
+  3. Answer the prompts that follow.
+  4. A sampleREADME.md will be generated and stored in "utils" folder.
+
+## URL
